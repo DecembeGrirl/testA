be7d55e8bef9fb32165fcaa1a5d1c0d29a89cc03
yangshuyuan <yangshuyuan@kkworld.com>
2021-03-02 14:48:15 +0800

    release 0.1.3
    Signed-off-by: yangshuyuan <yangshuyuan@kkworld.com>

---
+++ testA/testA.framework/Versions/A/Headers/YSYTestA.h
@@ -0,0 +1,17 @@
+//
+//  YSYTestA.h
+//  testA_Example
+//
+//  Created by yangshuyuan on 2021/2/3.
+//  Copyright © 2021 893833976@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YSYTestA : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
